e052db9a31948be435016d04c77c50b27839ad05
Robmeister2015 <A00226084@student.ait.ie>
2015-10-21 09:56:32 +0100

    committing.

---
+++ src/decimalFormatTest.java
@@ -0,0 +1,19 @@
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+
+public class decimalFormatTest {
+
+	public static void main(String[] args){
+		
+	DecimalFormat format = new DecimalFormat();
+	
+	format.setRoundingMode(RoundingMode.HALF_EVEN);
+	
+	double newDouble = 1.05555340595;
+	
+		
+	}
+	
+	
+}
